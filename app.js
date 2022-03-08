1236d2b7a0c2666fa1c38ca5f53477076199436b
Vedant Joshi <vedantj76@gmail.com>
2022-03-08 12:13:15 +0530

    added post with postman

--- app.js
+++ app.js
@@ -37,6 +37,26 @@ app.get("/articles",function(req,res)
     }
         
     });
+}); 
+
+
+app.post("/articles",function(req,res){
+    
+
+    const newArticle=new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function(err){
+        if(!err){
+            res.send("successfully added a new article.");
+        }
+        else{
+            res.send(err);
+        }
+    });
+
 });
 
 app.listen(3000, function() {
